9843c6692a1d77d142a3d847de1e657719d7c7f4
iandrei250 <aiordache020@gmail.com>
2024-12-24 17:44:16 +0100

    update:updated git ignore

--- HeroesApi/obj/Debug/net9.0/HeroesApi.AssemblyInfo.cs
+++ HeroesApi/obj/Debug/net9.0/HeroesApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeroesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bccdd3b48ecb105ee0187f9e52cf247077d305c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17b0232949850ceeac0f4ef311a2820e4698ff1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeroesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeroesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
